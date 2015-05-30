8d044e22d3e541bc2ee023e9a21570c4f400a0d9
Daniel Feichtinger <df@hyperledger.org>
2015-05-30 19:09:04 +0800

    Basic refactor to match new API

---
+++ lib/hyperledger_cli/key.rb
@@ -0,0 +1,40 @@
+require 'openssl'
+include OpenSSL
+
+module HyperledgerCli
+  class Key
+    def initialize(public_key)
+      @key = if public_key
+          PKey::EC.new(File.open(path(public_key)).read)
+        else
+          PKey::EC.new("secp256k1").generate_key
+        end
+    end
+    
+    def path(public_key)
+      root = "#{ENV['HOME']}/.hyperledger"
+      if public_key
+        "#{root}/#{public_key}.pem"
+      else
+        root
+      end
+    end
+    
+    def write
+      FileUtils.mkdir_p path
+      open path(public_key), 'w' do |io|
+        io.write @key.to_pem
+      end
+    end
+    
+    def public_key
+      @key.public_key.to_bn.to_s(16)
+    end
+    
+    def sign(message)
+      digest = Digest::SHA256.digest(message)
+      signature = @key.dsa_sign_asn1(digest)
+      signature.unpack("H*").first.upcase
+    end
+  end
+end
