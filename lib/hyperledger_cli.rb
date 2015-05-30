2353f3e3f203cf049d0b39f6cae87cf51cfd2cc5
Daniel Feichtinger <df@hyperledger.org>
2015-05-30 19:09:04 +0800

    Basic refactor to match new API

--- lib/hyperledger_cli.rb
+++ lib/hyperledger_cli.rb
@@ -1,4 +1,33 @@
-require "hyperledger_cli/version"
+require 'json'
+require 'restclient'
+require 'hyperledger_cli/version'
+require_relative 'hyperledger_cli/error_printer'
 
-module HyperledgerCli
+include HyperledgerCli::ErrorPrinter
+
+module HyperledgerCli  
+  def post(url, data, key = nil)
+    json_data = data.to_json
+    headers = {
+      content_type: :json,
+      accept: 'application/vnd.uber-amundsen+json'
+    }
+    if key
+      sig = key.sign(json_data)
+      headers.merge!({
+        authorization: "Hyper Key=#{key.public_key}, Signature=#{sig}"
+      })
+    end
+    
+    begin
+      response = JSON.parse(RestClient.post(url, json_data, headers))
+      response["uber"]["data"]
+    rescue => e
+      print_error(e)
+    end
+  end
+  
+  def get(url)
+    JSON.parse(RestClient.get(url, accept: 'application/vnd.uber-amundsen+json'))
+  end
 end
--- lib/hyperledger_cli/error_printer.rb
+++ lib/hyperledger_cli/error_printer.rb
@@ -1,11 +1,10 @@
+require 'pp'
+
 module HyperledgerCli
   module ErrorPrinter
     def print_error(err)
       puts "Error: #{err}"
-      errors = JSON.parse(err.response, symbolize_names: true)[:errors]
-      if errors
-        errors.each do |k, v| puts "#{k}:\t#{v.join(', ')}" end
-      end
+      pp err.response
     end
   end
 end
